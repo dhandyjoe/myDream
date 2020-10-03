848de063dcd48102539c292f0611f3bfb93168ce
Dhandy Joenathan Kurnia Putra <dhanjoenkp@gmail.com>
2020-10-03 16:14:01 +0700

    Update helloWorld.kt

--- helloWorld.kt
+++ helloWorld.kt
@@ -1,6 +1,23 @@
 fun main {
-  val a = 10
-  val b = 20
   
-  println(a+b)
+  println("Hello World")
+  
+  // Bilangan Genap
+  
+  val a: = 1..10
+  for(numbers in a) {
+    if ( numbers % 2 = 0){
+      println(numbers)
+    }
+  }
+  
+  // Bilangan Genap
+  
+  val a: = 1..10
+  for(numbers in a) {
+    if ( numbers % 2 != 0){
+      println(numbers)
+    }
+  }
+  
 }
